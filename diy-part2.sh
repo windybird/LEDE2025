773832b2fee1aaab211692bbd9e4e0cb01b6dc09
windybird <windybird@qq.com>
2025-12-10 21:23:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -68,6 +68,8 @@ mv files/unm/server.key $NAME/core/server.key
 #curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.crt -o $NAME/core/server.crt
 #curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.key -o $NAME/core/server.key
 
+ls $NAME/core
+
 # lucky
 rm -rf feeds/luci/applications/luci-app-lucky
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
